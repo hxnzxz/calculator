9287e983ca754ce4d5de9ec3533125837004e1f5
Devon <devonwu40@gmail.com>
2024-07-31 16:41:01 -0600

    Added compelte calculator functionality -handles errors -can do chain operations

--- scripts.js
+++ scripts.js
@@ -1,11 +1,37 @@
 let opperand1 = "";
 let opperand2 = "";
-let operator;
+let operator = null;
 let display = 0;
 let secondary = false;
+let hax = false;
 
 const displayContainer = document.querySelector('#display');
 displayContainer.textContent = display; 
+
+const equals = document.querySelector('#eq');
+equals.addEventListener('click', () => {
+    if(operator == null || opperand2 === ""){
+        updateDisplay(opperand1);
+    }else{
+        operate();
+        updateDisplay(display);
+        opperand2 = "";
+        if(hax){
+            hax = false;
+            display = 0;
+        }
+    }
+})
+
+const clear = document.querySelector('#clear');
+clear.addEventListener('click', () => {
+    opperand1 = "";
+    opperand2 = "";
+    operator = null;
+    display = 0;
+    secondary = false;
+    updateDisplay(display);
+})
 const numButtons = document.querySelectorAll('.numbers');
 numButtons.forEach(button => {
     button.addEventListener('click',(e) => {
@@ -22,8 +48,12 @@ numButtons.forEach(button => {
 const operatorButtons = document.querySelectorAll('.operators');
 operatorButtons.forEach(button => {
     button.addEventListener('click', (e) => {
+        if(operator !== null){
+            equals.click();
+        }
         secondary = true;
         operator = e.target.textContent;
+        // updateDisplay(displayContainer.textContent + operator);
     })
 })
 
@@ -32,15 +62,35 @@ function updateDisplay(newDisplay){
 }
 
 function operate(){
-    if(operator == '+'){
-        display += opperand1 + opperand2;
-    }else if(operator == "-"){
-        display += opperand1 - opperand2;
-    }else if(operator == "/"){
-        if(opperand2 === 0){
-            display = 'ERROR';
-        }else{
-            display += opperand1 / opperand2;
-        }
-    }
+    switch(operator){
+        case "+":
+            opperand1 = +opperand1 + +opperand2;
+            opperand1 = opperand1.toString();
+            display = opperand1;
+            break;
+        case "-":
+            opperand1 = +opperand1 - +opperand2;
+            opperand1 = opperand1.toString();
+            display = opperand1;
+            break;
+        case "/":
+            if(opperand2 ==='0'){
+                display = 'DENIED';
+                opperand1 = "";
+                operator = null;
+                hax = true;
+            }else{
+                opperand1 = +opperand1 / +opperand2;
+                opperand1 = opperand1.toString();
+                display = opperand1;
+            }
+            break;
+        case "*":
+            opperand1 = +opperand1 * +opperand2;
+            opperand1 = opperand1.toString();
+            display = opperand1;
+            break;
+    }   
+
+
 }
